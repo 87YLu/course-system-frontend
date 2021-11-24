ae4223ea7ab811f9bdcc57ed608bb3ce0218c696
87YLu <1790599287@qq.com>
2021-11-24 15:39:27 +0800

    chore(inti)：初始化项目

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  eslintIntegration: true,
+  stylelintIntegration: true,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+}
